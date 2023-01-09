24b309f3a0b20b91e6581dd12510478f1b1756ae
lifthus <lifthus531@gmail.com>
2023-01-09 16:04:41 +0900

    Redaer and Writer

--- goIO/main.go
+++ goIO/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"goIO/countIO"
+	"goIO/gzipRead"
 )
 
 func main() {
+	fmt.Println("CountLetters:")
 	countIO.UsingCountLetters()
+	fmt.Println("GzipReader")
+	gzipRead.UsingBuildGZipReader()
 }
