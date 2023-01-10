faecc5718850459b8c293dda3fb7c4e24c421b00
lifthus <lifthus531@gmail.com>
2023-01-10 17:00:09 +0900

    Handling json string stream encoding

--- handleJSON/streamEncoder/streamEncoder.go
+++ handleJSON/streamEncoder/streamEncoder.go
@@ -1,6 +1,7 @@
 package streamEncoder
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"strings"
@@ -8,13 +9,16 @@ import (
 	"handleJSON/tmpEncoder"
 )
 
+type Person tmpEncoder.Person
+
 func UsingStreamEncoder() {
 	data := `{"name":"Fred", "age":40}
 	{"name":"Mary", "age":21}
 	{"name":"Pat", "age":30}`
 
-	var t tmpEncoder.Person
+	var t Person
 
+	fmt.Println(" streamEncoder : Decoding ")
 	dec := json.NewDecoder(strings.NewReader(data))
 	for dec.More() { // saving each single jason at t at each time.
 		err := dec.Decode(&t)
@@ -24,4 +28,19 @@ func UsingStreamEncoder() {
 		// Processing Decoded json
 		fmt.Println(t)
 	}
+
+	fmt.Println(" streamEncoder : Encoding ")
+	data2 := make([]Person, 0)
+	data2 = append(data2, Person{Name: "GOPHER", Age: 13}, Person{Name: "GOLANG", Age: 31})
+	// Encoding
+	var b bytes.Buffer
+	enc := json.NewEncoder((&b))
+	for _, input := range data2 {
+		t = input
+		err := enc.Encode(t)
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println(b.String())
 }
