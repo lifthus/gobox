a79c7ccdc39debfee288800d9c36b61e2e33c257
lifthus <lifthus531@gmail.com>
2023-01-21 15:27:59 +0900

    handling string with unsafe package

---
+++ goUnsafe/handlingString/handlingString.go
@@ -0,0 +1,36 @@
+package hadnlingString
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"unsafe"
+)
+
+func GetStringHeader() {
+	s := "hello"
+	sHdr := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	fmt.Println(sHdr.Len) // 5
+
+	for i := 0; i < sHdr.Len; i++ {
+		bp := *(*byte)(unsafe.Pointer(sHdr.Data + uintptr(i)))
+		fmt.Println(string(bp))
+	}
+	fmt.Println()
+	runtime.KeepAlive(s)
+}
+
+func GetSliceHeasder() {
+	s := []int{10, 20, 30}
+	sHdr := (*reflect.SliceHeader)(unsafe.Pointer(&s))
+	fmt.Println(sHdr.Len)
+	fmt.Println(sHdr.Cap)
+
+	intByteSize := unsafe.Sizeof(s[0])
+	fmt.Println(intByteSize)
+	for i := 0; i < sHdr.Len; i++ {
+		intVal := *(*int)(unsafe.Pointer(sHdr.Data + intByteSize*uintptr(i)))
+		fmt.Println(intVal)
+	}
+	runtime.KeepAlive(s)
+}
